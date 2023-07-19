357dacb97ea4004224bd1dc61efa1cf50bff34b3
shamsur rahman <shamsur.works@gmail.com>
2023-07-19 18:23:51 +0600

    3rd commit again

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,28 @@
 # from django.http import JsonResponse,HttpResponse
-from django.forms.models import model_to_dict
+# from django.forms.models import model_to_dict
 from products.models import Product
-import json
+# import json
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.serializers import ProductSerializer
+# from django.http import JsonResponse
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
 
     # if request.method!="POST":
     #     return Response({"detailed":"GET NOT ALLOWED"},status=405)
-
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data['id']=model_data.id
-        # data['title']=model_data.title
-        # data['content']=model_data.content
-        # data['price']=model_data.price
-        # data = model_to_dict(model_data, fields=['id','title', 'content', 'price','sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data, headers = {'content-type':'application/json'})
+    serializer=ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        instance=serializer.save()
+        print(instance)
+    # instance = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if instance:
+    #     # data['id']=model_data.id
+    #     # data['title']=model_data.title
+    #     # data['content']=model_data.content
+    #     # data['price']=model_data.price
+    #     # data = model_to_dict(model_data, fields=['id','title', 'content', 'price','sale_price'])
+    #     data = ProductSerializer(instance).data
+        return Response(serializer.data)
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount= serializers.SerializerMethodField(read_only =True)
     class Meta:
         model= Product
         fields=[
@@ -10,5 +11,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'get_discount'
+            'my_discount'
         ]
+    def get_my_discount(self, obj):
+        return obj.get_discount()
